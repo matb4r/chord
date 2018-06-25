079450f6b674634756faa8f2dad2b58904db25b3
matb4r <matb4r@gmail.com>
2018-06-25 15:08:30 +0200

    find_successor

---
+++ src/com/mb/Main.java
@@ -0,0 +1,120 @@
+package com.mb;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    static final int m = 6;
+    static final int[] ids = {1, 3, 8, 11, 15, 22, 35};
+    static ArrayList<Node> nodes = initNodes();
+
+    private static ArrayList<Node> initNodes() {
+        ArrayList<Node> nodes = new ArrayList<>();
+        for (int id : ids) {
+            nodes.add(new Node(id));
+        }
+        return nodes;
+    }
+
+    public static Node idToNode(int id) {
+        for (Node n : nodes) {
+            if (n.n == id) {
+                return n;
+            }
+        }
+        throw new RuntimeException("There is no Node with n=" + id);
+    }
+
+    public static void main(String[] args) {
+        testFindSuccessor(false);
+    }
+
+    public static void testFindSuccessor(boolean debug) {
+        for (Node testNode : nodes) {
+            System.out.println("Node " + testNode.n);
+            testFindSuccessor(testNode, 0, 1, debug);
+            testFindSuccessor(testNode, 1, 1, debug);
+            testFindSuccessor(testNode, 2, 3, debug);
+            testFindSuccessor(testNode, 3, 3, debug);
+            testFindSuccessor(testNode, 4, 8, debug);
+            testFindSuccessor(testNode, 5, 8, debug);
+            testFindSuccessor(testNode, 6, 8, debug);
+            testFindSuccessor(testNode, 7, 8, debug);
+            testFindSuccessor(testNode, 8, 8, debug);
+            testFindSuccessor(testNode, 9, 11, debug);
+            testFindSuccessor(testNode, 10, 11, debug);
+            testFindSuccessor(testNode, 11, 11, debug);
+            testFindSuccessor(testNode, 12, 15, debug);
+            testFindSuccessor(testNode, 13, 15, debug);
+            testFindSuccessor(testNode, 14, 15, debug);
+            testFindSuccessor(testNode, 15, 15, debug);
+            testFindSuccessor(testNode, 16, 22, debug);
+            testFindSuccessor(testNode, 17, 22, debug);
+            testFindSuccessor(testNode, 18, 22, debug);
+            testFindSuccessor(testNode, 19, 22, debug);
+            testFindSuccessor(testNode, 20, 22, debug);
+            testFindSuccessor(testNode, 21, 22, debug);
+            testFindSuccessor(testNode, 22, 22, debug);
+            testFindSuccessor(testNode, 23, 35, debug);
+            testFindSuccessor(testNode, 24, 35, debug);
+            testFindSuccessor(testNode, 25, 35, debug);
+            testFindSuccessor(testNode, 26, 35, debug);
+            testFindSuccessor(testNode, 27, 35, debug);
+            testFindSuccessor(testNode, 28, 35, debug);
+            testFindSuccessor(testNode, 29, 35, debug);
+            testFindSuccessor(testNode, 30, 35, debug);
+            testFindSuccessor(testNode, 31, 35, debug);
+            testFindSuccessor(testNode, 32, 35, debug);
+            testFindSuccessor(testNode, 33, 35, debug);
+            testFindSuccessor(testNode, 34, 35, debug);
+            testFindSuccessor(testNode, 35, 35, debug);
+            testFindSuccessor(testNode, 36, 1, debug);
+            testFindSuccessor(testNode, 37, 1, debug);
+            testFindSuccessor(testNode, 38, 1, debug);
+            testFindSuccessor(testNode, 39, 1, debug);
+            testFindSuccessor(testNode, 40, 1, debug);
+            testFindSuccessor(testNode, 41, 1, debug);
+            testFindSuccessor(testNode, 42, 1, debug);
+            testFindSuccessor(testNode, 43, 1, debug);
+            testFindSuccessor(testNode, 44, 1, debug);
+            testFindSuccessor(testNode, 45, 1, debug);
+            testFindSuccessor(testNode, 46, 1, debug);
+            testFindSuccessor(testNode, 47, 1, debug);
+            testFindSuccessor(testNode, 48, 1, debug);
+            testFindSuccessor(testNode, 49, 1, debug);
+            testFindSuccessor(testNode, 50, 1, debug);
+            testFindSuccessor(testNode, 51, 1, debug);
+            testFindSuccessor(testNode, 52, 1, debug);
+            testFindSuccessor(testNode, 53, 1, debug);
+            testFindSuccessor(testNode, 54, 1, debug);
+            testFindSuccessor(testNode, 55, 1, debug);
+            testFindSuccessor(testNode, 56, 1, debug);
+            testFindSuccessor(testNode, 57, 1, debug);
+            testFindSuccessor(testNode, 58, 1, debug);
+            testFindSuccessor(testNode, 59, 1, debug);
+            testFindSuccessor(testNode, 60, 1, debug);
+            testFindSuccessor(testNode, 61, 1, debug);
+            testFindSuccessor(testNode, 62, 1, debug);
+            testFindSuccessor(testNode, 63, 1, debug);
+            System.out.println("  ok");
+        }
+        System.out.println("Done! All ok!");
+    }
+
+    public static void testFindSuccessor(Node node, int find, int expected, boolean debug) {
+        int x = node.find_successor(find);
+        if (x != expected) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            throw new RuntimeException("find_successor(" + node.n + ", " + find + ", " + expected + "): should get " + expected + " but got " + x);
+        } else {
+            if (debug) {
+                System.out.println("\t" + find + " -> " + expected);
+            }
+        }
+    }
+
+}
---
+++ src/com/mb/Node.java
@@ -0,0 +1,99 @@
+package com.mb;
+
+import java.util.ArrayList;
+
+import static com.mb.Main.*;
+
+class Node {
+
+    public int n; // node/group id
+    String ip; // node ip
+    ArrayList<String> ips; // all ips in group
+
+    private Finger[] fingerTable;
+    private int successor;
+    private int predecessor;
+
+    Node(int n) {
+        this.n = n;
+        this.fingerTable = new Finger[m];
+        for (int i = 1; i <= m; i++) {
+            fingerTable[i - 1] = new Finger();
+            fingerTable[i - 1].i = i;
+            fingerTable[i - 1].start = (n + (int) Math.pow(2, i - 1)) % (int) Math.pow(2, m);
+            fingerTable[i - 1].end = (n + (int) Math.pow(2, i) - 1) % (int) Math.pow(2, m);
+            fingerTable[i - 1].node = successor(fingerTable[i - 1].start);
+        }
+        this.successor = fingerTable[0].node;
+        this.predecessor = predecessor(n);
+    }
+
+    private static int successor(int id) {
+        for (int i = 0; i < ids.length - 1; i++) {
+            if (ids[i + 1] >= id && ids[i] < id) {
+                return ids[i + 1];
+            }
+        }
+        return ids[0];
+    }
+
+    private int predecessor(int id) {
+        for (int i = 1; i < ids.length; i++) {
+            if (this.n == ids[i]) {
+                return ids[i - 1];
+            }
+        }
+        return ids[ids.length - 1];
+    }
+
+    public int find_successor(int id) {
+        if (id >= Math.pow(2, m)) {
+            throw new RuntimeException("Id [" + id + "] too big for the identifier space");
+        }
+        if (id > predecessor && id <= n) {
+            return n;
+        }
+        int np = find_predecessor(id);
+        return idToNode(np).successor;
+    }
+
+    private int find_predecessor(int id) {
+        int np = this.n;
+        while (!isIdBetweenNAndNSucc(id, np, idToNode(np).successor)) {
+            np = idToNode(np).closest_preceding_finger(id);
+        }
+        return np;
+    }
+
+    private boolean isIdBetweenNAndNSucc(int id, int np, int np_succ) {
+        if (np < np_succ) {
+            return id > np && id <= np_succ;
+        } else {
+            return id <= np_succ || id > np;
+        }
+    }
+
+    private int closest_preceding_finger(int id) {
+        for (int i = m - 1; i >= 0; i--) {
+            if (n == id) {
+                return this.predecessor;
+            } else if (n > id) {
+                if (fingerTable[i].node > this.n || fingerTable[i].node < id) {
+                    return fingerTable[i].node;
+                }
+            } else {
+                if (fingerTable[i].node > this.n && fingerTable[i].node < id) {
+                    return fingerTable[i].node;
+                }
+            }
+        }
+        return this.predecessor;
+    }
+
+    class Finger {
+        int i;
+        int start;
+        int end;
+        int node;
+    }
+}
