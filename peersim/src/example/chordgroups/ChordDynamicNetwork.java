a72e002838c717cc37e23eaa502e9e9ca3d85ef0
matb4r <matb4r@gmail.com>
2018-08-22 11:46:01 +0200

    random dynamic net

--- peersim/src/example/chordgroups/ChordDynamicNetwork.java
+++ peersim/src/example/chordgroups/ChordDynamicNetwork.java
@@ -1,11 +1,16 @@
 package example.chordgroups;
 
+import peersim.config.Configuration;
 import peersim.core.CommonState;
 import peersim.core.Network;
 import peersim.core.Node;
 import peersim.dynamics.DynamicNetwork;
 
 public class ChordDynamicNetwork extends DynamicNetwork {
+
+    private static final String PAR_RANDOM = "random";
+
+    private boolean random = false;
 
     int addPositive = (int) add >= 0 ? (int) add : (int) -add;
     boolean isAddNow = (int) add >= 0;
@@ -15,30 +20,39 @@ public class ChordDynamicNetwork extends DynamicNetwork {
 
     public ChordDynamicNetwork(String prefix) {
         super(prefix);
+        random = Configuration.contains(prefix + "." + PAR_RANDOM);
     }
 
     public final boolean execute() {
-        if (isAddNow) {
-            try {
-                if (Network.size() < maxsize)
-                    add(1);
-                addCounter--;
-                if (addCounter == 0) {
-                    addCounter = (int) addPositive;
-                    isAddNow = false;
-                }
-            } catch (Exception ex) {
+        if (random) {
+            if(CommonState.r.nextBoolean()) {
+                add(1);
+            } else {
+                remove(1);
             }
         } else {
-            try {
-                if (Network.size() > minsize)
-                    remove(1);
-                removeCounter--;
-                if (removeCounter == 0) {
-                    removeCounter = (int) addPositive;
-                    isAddNow = true;
+            if (isAddNow) {
+                try {
+                    if (Network.size() < maxsize)
+                        add(1);
+                    addCounter--;
+                    if (addCounter == 0) {
+                        addCounter = (int) addPositive;
+                        isAddNow = false;
+                    }
+                } catch (Exception ex) {
                 }
-            } catch (Exception ex) {
+            } else {
+                try {
+                    if (Network.size() > minsize)
+                        remove(1);
+                    removeCounter--;
+                    if (removeCounter == 0) {
+                        removeCounter = (int) addPositive;
+                        isAddNow = true;
+                    }
+                } catch (Exception ex) {
+                }
             }
         }
 
