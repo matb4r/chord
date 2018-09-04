7ee6d127a8bb4e1e31b1d055ff1ac94bf6e83209
matb4r <matb4r@gmail.com>
2018-09-04 14:58:35 +0200

    add metrics

--- peersim/src/example/staticgroups/StaticGroupsMetrics.java
+++ peersim/src/example/staticgroups/StaticGroupsMetrics.java
@@ -1,11 +1,22 @@
 package example.staticgroups;
 
+import peersim.config.Configuration;
 import peersim.core.Control;
 import peersim.core.Network;
 
 import java.time.LocalTime;
 
 public class StaticGroupsMetrics implements Control {
+
+    private static final String PAR_IDLENGTH = "idLength";
+    private static final String PAR_MAX_GROUP_SIZE = "maxGroupSize";
+    private static final String PAR_CYCLES = "cycles";
+    private static final String PAR_INIT_NETWORK_SIZE = "networkInitSize";
+
+    private int idLength = 0;
+    private int maxGroupSize = 0;
+    private int cycles = 0;
+    private int networkInitSize = 0;
 
     LocalTime started;
     LocalTime stopped;
@@ -14,6 +25,10 @@ public class StaticGroupsMetrics implements Control {
     static int exceptionsCounter = 0;
 
     public StaticGroupsMetrics(String prefix) {
+        idLength = Configuration.getInt(prefix + "." + PAR_IDLENGTH);
+        maxGroupSize = Configuration.getInt(prefix + "." + PAR_MAX_GROUP_SIZE);
+        cycles = Configuration.getInt(prefix + "." + PAR_CYCLES);
+        networkInitSize = Configuration.getInt(prefix + "." + PAR_INIT_NETWORK_SIZE);
     }
 
     @Override
@@ -28,11 +43,17 @@ public class StaticGroupsMetrics implements Control {
     }
 
     public void executeOnStart() {
-        started = LocalTime.now();
+        this.started = LocalTime.now();
+        this.cycles = cycles;
+        this.networkInitSize = networkInitSize;
     }
 
     public void executeOnEnd() {
         stopped = LocalTime.now();
+        System.out.println("cycles: " + cycles);
+        System.out.println("network init size: " + networkInitSize);
+        System.out.println("m: " + idLength);
+        System.out.println("max group size: " + maxGroupSize);
         System.out.println("started " + started);
         System.out.println("stopped " + stopped);
         System.out.println("time: " + stopped.minusNanos(started.toNanoOfDay()));
